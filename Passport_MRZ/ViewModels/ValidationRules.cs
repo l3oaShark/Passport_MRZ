5c7e2c446116744e628cf7c68b183b28220cbb9f
!SharkPongsatorn <33623726+l3oaShark@users.noreply.github.com>
2021-07-12 14:50:05 +0700

    solve datepicker  issue date selectof issue date

---
+++ Passport_MRZ/ViewModels/ValidationRules.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Passport_MRZ.ViewModels
+{
+   public class ValidationRules : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+
+            if (value is int)
+            {
+                int i;
+                if (int.TryParse(value.ToString(), out i))
+                {
+                    return new ValidationResult(true, null);
+                }
+                else
+                {
+                    return new ValidationResult(false, "Please enter a valid value.");
+                }
+            }
+            else if (value is string)
+            {
+                string a;
+                if (value.ToString().Length > 0)
+                {
+                    return new ValidationResult(true, null);
+                }
+                else
+                {
+                    return new ValidationResult(false, "Please enter a valid value.");
+                }
+
+            }
+            else if (value is DateTime)
+            {
+                return DateTime.TryParse((value ?? "").ToString(),
+              CultureInfo.CurrentCulture,
+              DateTimeStyles.AssumeLocal | DateTimeStyles.AllowWhiteSpaces,
+              out _)
+              ? ValidationResult.ValidResult
+              : new ValidationResult(false, "Invalid date");
+            }
+            return new ValidationResult(false, "Please enter a value.");
+        }
+    }
+}
